1dd06c8100421c836467e497fc451fd39fd23bc2
BenC0 <suddensquirrel94@gmail.com>
2020-09-16 17:13:47 +0100

    Header built

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
 import './App.css';
+import React from 'react';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-        <h1>Simon Creates .online</h1>
-    </div>
+  	<Header />
   );
 }
 
